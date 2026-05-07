4b9344ee6eb2aaddb8839774dd6ebc33ed82f1d1
Reem-Ashraff <115889280+Reem-Ashraff@users.noreply.github.com>
2026-05-07 23:51:35 +0300

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -16,7 +16,7 @@ export class ApiService {
   baseURL = "https://greenstore.infinityfreeapp.com/api/get_plants.php";
   categoriesURL = "https://greenstore.infinityfreeapp.com/api/get_categories.php";
   insertURL="https://greenstore.infinityfreeapp.com/api/signup_api.php";
-  loginURL = "/proxy-api/login.php";
+  loginURL = "http://greenstore-api.atwebpages.com/login.php";
   insertCartURL = "https://greenstore.infinityfreeapp.com/api/insertToCart.php";
   userURL = "https://greenstore-api.atwebpages.com/get_user.php";
   getcartURL = "https://greenstore-api.thsite.top/get_cart.php";
