a8aefcdca5ad3c87d9629d3416c672747312cfeb
Reem-Ashraff <115889280+Reem-Ashraff@users.noreply.github.com>
2026-05-07 17:03:07 +0300

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -16,10 +16,10 @@ export class ApiService {
   baseURL = "https://greenstore.infinityfreeapp.com/api/get_plants.php";
   categoriesURL = "https://greenstore.infinityfreeapp.com/api/get_categories.php";
   insertURL="https://greenstore.infinityfreeapp.com/api/signup_api.php";
-  loginURL = "https://greenstore.infinityfreeapp.com/api/login.php";
+  loginURL = "https://greenstore-api.thsite.top/login.php";
   insertCartURL = "https://greenstore.infinityfreeapp.com/api/insertToCart.php";
-  userURL = "https://greenstore.infinityfreeapp.com/api/get_user.php";
-  getcartURL = "https://greenstore.infinityfreeapp.com/api/get_cart.php";
+  userURL = "https://greenstore-api.thsite.top/get_user.php";
+  getcartURL = "https://greenstore-api.thsite.top/get_cart.php";
   deleteitemURL = "https://greenstore.infinityfreeapp.com/api/delete_cartItem.php";
   deleteplantURL = "https://greenstore.infinityfreeapp.com/api/delete_plant.php";
   addcategoryURL = "https://greenstore.infinityfreeapp.com/add_category.php";
