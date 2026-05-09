1bbc8d97b6d629fe38dc8b511fc7ad975ff0895b
Reem-Ashraff <115889280+Reem-Ashraff@users.noreply.github.com>
2026-05-09 18:36:41 +0300

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -44,22 +44,23 @@ export class LoginComponent {
         this.msg="you loged in successfully.";
         localStorage.setItem("log","Log out");
         this.apiservice.user_id = this.res["user_id"];
-        this.apiservice.get_user(this.apiservice.user_id)
-        .subscribe({next:(data:any)=>{
-          this.apiservice.userdata = data;
-          this.apiservice.storeuser=localStorage.setItem("username",this.apiservice.userdata[0]["username"]);
-          this.apiservice.user_id = localStorage.setItem("user_id",this.apiservice.user_id);
-          localStorage.setItem("user_role",this.apiservice.userdata[0]["type"]);
-          
-        }})
-        this.apiservice.get_cart(this.apiservice.user_id)
-        .subscribe({next:(data:any)=>{
-          this.cartProducts = data
-          this.products = localStorage.setItem("cart_items",JSON.stringify(this.cartProducts))
-          localStorage.setItem("items_num",this.cartProducts.length)
-          //this.router.navigate(["/home"]);
-        }})
         this.router.navigate(["/home"]);
+        //this.apiservice.get_user(this.apiservice.user_id)
+        //.subscribe({next:(data:any)=>{
+          //this.apiservice.userdata = data;
+          //this.apiservice.storeuser=localStorage.setItem("username",this.apiservice.userdata[0]["username"]);
+          //this.apiservice.user_id = localStorage.setItem("user_id",this.apiservice.user_id);
+          //localStorage.setItem("user_role",this.apiservice.userdata[0]["type"]);
+          
+        //}})
+        //this.apiservice.get_cart(this.apiservice.user_id)
+        //.subscribe({next:(data:any)=>{
+          //this.cartProducts = data
+          //this.products = localStorage.setItem("cart_items",JSON.stringify(this.cartProducts))
+          //localStorage.setItem("items_num",this.cartProducts.length)
+          //this.router.navigate(["/home"]);
+        //}})
+        //this.router.navigate(["/home"]);
       }
       else if(this.res["message"]=="Invalid password"){
         this.msg2="Invalid password.";
